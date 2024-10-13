4a3a45385479f7510a9013c270a8a84370e398f1
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-10-14 02:31:21 +0530

    Nextjs Installation

--- annual-sync/src/app/layout.tsx
+++ annual-sync/src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,18 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
   );
 }
--- annual-sync/src/appwrite/config.ts
+++
@@ -1,9 +0,0 @@
-import { Client, Account, Databases } from "appwrite";
-
-const client = new Client().setProject('670c225300290452f2fc')
-
-const account = new Account(client)
-
-const databases = new Databases(client)
-
-export { client, account, databases }
