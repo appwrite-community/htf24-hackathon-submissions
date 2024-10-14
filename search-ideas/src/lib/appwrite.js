64a7194ea2a73b58a252dd6e9f1dce4ce01e53cd
leonyangela <38250310+leonyangela@users.noreply.github.com>
2024-10-14 16:33:48 +0700

    update submission_template

--- search-ideas/src/lib/appwrite.js
+++
@@ -1,9 +0,0 @@
-import { Client, Databases, Account } from "appwrite";
-
-const client = new Client();
-client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("66fe00d80036b3de7770"); // Replace with your project ID
-
-export const account = new Account(client);
-export const databases = new Databases(client);
